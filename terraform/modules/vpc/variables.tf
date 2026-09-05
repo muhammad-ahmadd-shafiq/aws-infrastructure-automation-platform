c467accb035c8e03ca093ef5f5a4e64272237a87
Ahmad <2023f-mulbsit-036@mul.edu.pk>
2026-09-05 19:13:28 +0500

    Terraform VPC Module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
